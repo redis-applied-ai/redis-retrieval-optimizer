ed99a49e904a9c66e08ab66a9210ce06dae9a06b
Robert Shelton <robert.shelton@redis.com>
2025-07-14 15:54:30 -0400

    Add release workflow make dtype part of grid_study (#23)

--- redis_retrieval_optimizer/grid_study.py
+++ redis_retrieval_optimizer/grid_study.py
@@ -15,7 +15,12 @@ from redis_retrieval_optimizer.search_methods import SEARCH_METHOD_MAP
 
 
 def update_metric_row(
-    metrics, grid_study_config, search_method, embedding_settings, trial_metrics: dict
+    metrics,
+    grid_study_config,
+    search_method,
+    embedding_settings,
+    trial_metrics: dict,
+    vector_data_type: str = None,
 ):
     metrics["search_method"].append(search_method)
     metrics["ret_k"].append(grid_study_config.ret_k)
@@ -25,7 +30,7 @@ def update_metric_row(
     metrics["m"].append(grid_study_config.index_settings.m)
     metrics["distance_metric"].append(grid_study_config.index_settings.distance_metric)
     metrics["vector_data_type"].append(
-        grid_study_config.index_settings.vector_data_type
+        vector_data_type or grid_study_config.index_settings.vector_data_type
     )
     metrics["model"].append(embedding_settings.model)
     metrics["model_dim"].append(embedding_settings.dim)
@@ -44,11 +49,19 @@ def persist_metrics(metrics, redis_url, study_id):
 
 
 def init_index_from_grid_settings(
-    grid_study_config: GridStudyConfig, redis_url: str, corpus_processor: Callable
+    grid_study_config: GridStudyConfig,
+    redis_url: str,
+    corpus_processor: Callable,
+    dtype: str = None,
 ) -> SearchIndex:
     index_settings = grid_study_config.index_settings.model_dump()
     embed_settings = grid_study_config.embedding_models[0]
     index_settings["embedding"] = embed_settings.model_dump()
+
+    # Use provided dtype or default from embedding model
+    if dtype:
+        index_settings["vector_data_type"] = dtype
+        embed_settings.dtype = dtype
 
     if grid_study_config.index_settings.from_existing:
         print(f"Connecting to existing index: {grid_study_config.index_settings.name}")
@@ -65,10 +78,12 @@ def init_index_from_grid_settings(
         )
         if (
             embed_settings.dim
-            != index.schema.fields[grid_study_config.vector_field_name].attrs.dims
+            != index.schema.fields[
+                grid_study_config.index_settings.vector_field_name
+            ].attrs.dims
         ):
             raise ValueError(
-                f"Embedding model dimension {emb_model.dims} does not match index dimension {index.schema.fields[grid_study_config.vector_field_name].attrs['dims']}"
+                f"Embedding model dimension {embed_settings.dim} does not match index dimension {index.schema.fields[grid_study_config.index_settings.vector_field_name].attrs['dims']}"
             )
         utils.set_last_index_settings(redis_url, index_settings)
     else:
@@ -77,16 +92,19 @@ def init_index_from_grid_settings(
             index_settings, last_index_settings
         )
 
-        schema = utils.schema_from_settings(
-            grid_study_config.index_settings,
-        )
+        # Create a copy of index settings with current dtype
+        current_index_settings = grid_study_config.index_settings.model_copy()
+        if dtype:
+            current_index_settings.vector_data_type = dtype
+
+        schema = utils.schema_from_settings(current_index_settings)
 
         index = SearchIndex.from_dict(schema, redis_url=redis_url)
         index.create(overwrite=recreate_index, drop=recreate_data)
 
         if recreate_data:
             emb_model = utils.get_embedding_model(
-                grid_study_config.embedding_models[0], redis_url
+                grid_study_config.embedding_models[0], redis_url, dtype=dtype
             )
             print("Recreating: loading corpus from file")
             corpus = utils.load_json(grid_study_config.corpus)
@@ -117,10 +135,6 @@ def run_grid_study(
     queries = utils.load_json(grid_study_config.queries)
     qrels = Qrels(utils.load_json(grid_study_config.qrels))
 
-    index = init_index_from_grid_settings(
-        grid_study_config, redis_url, corpus_processor
-    )
-
     metrics: dict = {
         "search_method": [],
         "total_indexing_time": [],
@@ -141,73 +155,87 @@ def run_grid_study(
     }
 
     for i, embedding_model in enumerate(grid_study_config.embedding_models):
-        if i > 0:
-            # assuming that you didn't pass the same embedding model twice like a fool
-            print("Recreating index with new embedding model")
+        for dtype in grid_study_config.vector_data_types:
+            # Update index settings for current dtype
+            current_index_settings = grid_study_config.index_settings.model_copy()
+            current_index_settings.vector_data_type = dtype
 
-            # delete old index and data with embedding cache it's not expensive to recreate
-            # consider potential of pre-fixing studies with study_id for separation
-            index_settings = grid_study_config.index_settings
+            # Create or get index for current settings
+            if i == 0 and dtype == grid_study_config.vector_data_types[0]:
+                # First iteration - initialize index
+                index = init_index_from_grid_settings(
+                    grid_study_config, redis_url, corpus_processor, dtype=dtype
+                )
+            else:
+                # Recreate index with new settings
+                print(f"Recreating index with dtype: {dtype}")
 
-            # assign new vector info to index_settings
-            index_settings.vector_data_type = embedding_model.dtype
-            index_settings.vector_dim = embedding_model.dim
+                # Update index settings for current embedding model and dtype
+                current_index_settings.vector_dim = embedding_model.dim
 
-            schema = utils.schema_from_settings(index_settings)
-            index = utils.index_from_schema(
-                schema, redis_url, recreate_index=True, recreate_data=True
+                schema = utils.schema_from_settings(current_index_settings)
+                index = utils.index_from_schema(
+                    schema, redis_url, recreate_index=True, recreate_data=True
+                )
+
+                print("Recreating: loading corpus from file")
+                emb_model = utils.get_embedding_model(
+                    embedding_model, redis_url, dtype=dtype
+                )
+                corpus = utils.load_json(grid_study_config.corpus)
+
+                # corpus processing functions should be user defined
+                corpus_data = corpus_processor(corpus, emb_model)
+                index.load(corpus_data)
+
+                while float(index.info()["percent_indexed"]) < 1:
+                    time.sleep(1)
+                    logging.info(
+                        f"Indexing progress: {index.info()['percent_indexed']}"
+                    )
+
+            # Get embedding model with current dtype
+            emb_model = utils.get_embedding_model(
+                embedding_model, redis_url, dtype=dtype
             )
 
-            # TODO: be able to dump existing index corpus to file automatically which shouldn't be too hard
-            print(
-                "If using multiple embedding models assuming there is a json version of corpus available."
-            )
-            print("Recreating: loading corpus from file")
-            emb_model = utils.get_embedding_model(embedding_model, redis_url)
-            corpus = utils.load_json(grid_study_config.corpus)
+            for search_method in grid_study_config.search_methods:
+                print(f"Running search method: {search_method} with dtype: {dtype}")
+                # get search method to try
+                search_fn = search_method_map[search_method]
+                search_input = SearchMethodInput(
+                    index=index,
+                    raw_queries=queries,
+                    emb_model=emb_model,
+                    id_field_name=grid_study_config.index_settings.id_field_name,
+                    vector_field_name=grid_study_config.index_settings.vector_field_name,
+                    text_field_name=grid_study_config.index_settings.text_field_name,
+                )
 
-            # corpus processing functions should be user defined
-            corpus_data = corpus_processor(corpus, emb_model)
-            index.load(corpus_data)
+                search_method_output = search_fn(search_input)
 
-            while float(index.info()["percent_indexed"]) < 1:
-                time.sleep(1)
-                logging.info(f"Indexing progress: {index.info()['percent_indexed']}")
+                trial_metrics = utils.eval_trial_metrics(
+                    qrels, search_method_output.run
+                )
+                trial_metrics["total_indexing_time"] = round(
+                    float(index.info()["total_indexing_time"]) / 1000, 5
+                )
+                trial_metrics["query_stats"] = utils.get_query_time_stats(
+                    search_method_output.query_metrics.query_times
+                )
 
-        # check if matches with last index settings
-        emb_model = utils.get_embedding_model(embedding_model, redis_url)
+                # Create embedding settings with current dtype for metrics
+                embedding_settings_with_dtype = embedding_model.model_copy()
+                embedding_settings_with_dtype.dtype = dtype
 
-        for search_method in grid_study_config.search_methods:
-            print(f"Running search method: {search_method}")
-            # get search method to try
-            search_fn = search_method_map[search_method]
-            search_input = SearchMethodInput(
-                index=index,
-                raw_queries=queries,
-                emb_model=emb_model,
-                id_field_name=grid_study_config.index_settings.id_field_name,
-                vector_field_name=grid_study_config.index_settings.vector_field_name,
-                text_field_name=grid_study_config.index_settings.text_field_name,
-            )
+                metrics = update_metric_row(
+                    metrics,
+                    grid_study_config,
+                    search_method,
+                    embedding_settings_with_dtype,
+                    trial_metrics,
+                    vector_data_type=dtype,
+                )
 
-            search_method_output = search_fn(search_input)
-
-            trial_metrics = utils.eval_trial_metrics(qrels, search_method_output.run)
-            trial_metrics["total_indexing_time"] = round(
-                float(index.info()["total_indexing_time"]) / 1000, 5
-            )
-            trial_metrics["query_stats"] = utils.get_query_time_stats(
-                search_method_output.query_metrics.query_times
-            )
-
-            metrics = update_metric_row(
-                metrics,
-                grid_study_config,
-                search_method,
-                embedding_model,
-                trial_metrics,
-            )
-
-            persist_metrics(metrics, redis_url, grid_study_config.study_id)
-
+                persist_metrics(metrics, redis_url, grid_study_config.study_id)
     return pd.DataFrame(metrics)
--- tests/integration/test_grid.py
+++ tests/integration/test_grid.py
@@ -20,6 +20,9 @@ def test_run_grid_study(redis_url):
     study_config["queries"] = f"{TEST_DIR}/grid_data/queries.json"
     study_config["qrels"] = f"{TEST_DIR}/grid_data/qrels.json"
 
+    # Add vector_data_types to test the new dtype functionality
+    study_config["vector_data_types"] = ["float32"]
+
     with open(config_path, "w") as f:
         yaml.dump(study_config, f)
 
@@ -29,9 +32,66 @@ def test_run_grid_study(redis_url):
         corpus_processor=eval_beir.process_corpus,
     )
 
-    assert metrics.shape[0] == len(study_config["search_methods"]) * len(
-        study_config["embedding_models"]
+    # Calculate expected number of trials: embedding_models * vector_data_types * search_methods
+    expected_trials = (
+        len(study_config["embedding_models"])
+        * len(study_config["vector_data_types"])
+        * len(study_config["search_methods"])
     )
+
+    assert metrics.shape[0] == expected_trials
+
+    for score in metrics["f1"].tolist():
+        assert score > 0.0
+
+    last_schema = utils.get_last_index_settings(redis_url)
+    assert last_schema is not None
+
+    index = SearchIndex.from_existing(last_schema["name"], redis_url=redis_url)
+
+    assert index.info()["num_docs"] == 5
+
+    # clean up
+    index.client.json().delete("ret-opt:last_schema")
+    index.delete(drop=True)
+
+
+def test_run_grid_study_with_multiple_dtypes(redis_url):
+    """Test grid study with multiple vector data types."""
+    config_path = f"{TEST_DIR}/grid_data/test_grid_study_config.yaml"
+
+    with open(config_path, "r") as f:
+        study_config = yaml.safe_load(f)
+
+    study_config["corpus"] = f"{TEST_DIR}/grid_data/corpus.json"
+    study_config["queries"] = f"{TEST_DIR}/grid_data/queries.json"
+    study_config["qrels"] = f"{TEST_DIR}/grid_data/qrels.json"
+
+    # Test with multiple dtypes
+    study_config["vector_data_types"] = ["float16", "float32"]
+
+    with open(config_path, "w") as f:
+        yaml.dump(study_config, f)
+
+    metrics = run_grid_study(
+        config_path=config_path,
+        redis_url=redis_url,
+        corpus_processor=eval_beir.process_corpus,
+    )
+
+    # Calculate expected number of trials: embedding_models * vector_data_types * search_methods
+    expected_trials = (
+        len(study_config["embedding_models"])
+        * len(study_config["vector_data_types"])
+        * len(study_config["search_methods"])
+    )
+
+    assert metrics.shape[0] == expected_trials
+
+    # Verify that both dtypes are present in the results
+    unique_dtypes = metrics["vector_data_type"].unique()
+    assert "float16" in unique_dtypes
+    assert "float32" in unique_dtypes
 
     for score in metrics["f1"].tolist():
         assert score > 0.0
